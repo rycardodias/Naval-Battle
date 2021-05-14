5c2eb3e982503e8aad7bda8da4a369391fd90485
Ricardo Dias <42947771+rycardodias@users.noreply.github.com>
2021-05-14 08:59:10 +0100

    .

--- src/ipvc/estg/prog2/games/batalhaNaval/Jogador.java
+++ src/ipvc/estg/prog2/games/batalhaNaval/Jogador.java
@@ -3,25 +3,7 @@ package ipvc.estg.prog2.games.batalhaNaval;
 import java.util.Scanner;
 
 public class Jogador {
-
-//    private static int[] jogar(int jogador, int linha, int coluna) {
-//        int venceu = 0;
-//        int jogadaValida = 0;
-//        int[] aux = new int[2];
-//
-//        jogadaValida = Tabuleiro.verificarJogada(linha, coluna, jogador);
-//
-//        if (jogadaValida == 1) {
-//            venceu = Tabuleiro.verificarVitoria(jogador);
-//        }
-//
-//        aux[0] = venceu;
-//        aux[1] = jogadaValida;
-//
-//        return aux;
-//    }
-
-    public static int jogarManual(int jogador) {
+        public static int jogarManual(int jogador) {
         System.out.println("\n\nJOGADOR " + jogador + " - INDIQUE 3 POSIÇOES PARA JOGAR");
         int i = 0;
         int linha, coluna;
